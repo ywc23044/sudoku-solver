a1b771712c0ecf596a24571659c75281b93c9bd1
ywc23044 <stchan23044@gmail.com>
2017-03-20 23:11:02 +0000

    refractor

--- sudoku/solver.js
+++ sudoku/solver.js
@@ -1,55 +1,107 @@
-var grid = [
-  [ 8, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-  [ 'x', 'x', 3, 6, 'x', 'x', 'x', 'x', 'x'],
-  [ 'x', 7, 'x', 'x', 9, 'x', 2, 'x', 'x'],
-  [ 'x', 5, 'x', 'x', 'x', 7, 'x', 'x', 'x'],
-  [ 'x', 'x', 'x', 'x', 4, 5, 7, 'x', 'x'],
-  [ 'x', 'x', 'x', 1, 'x', 'x', 'x', 3, 'x'],
-  [ 'x', 'x', 1, 'x', 'x', 'x', 'x', 6, 8],
-  [ 'x', 'x', 8, 5, 'x', 'x', 'x', 1, 'x'],
-  [ 'x', 9, 'x', 'x', 'x', 'x', 4, 'x', 'x']
-]
+document.getElementById('sudoku-grid').addEventListener('change', (e) => {
+  let target = e.target
+  let loc = {row : target.id[1], col : target.id[2]}
+  if(target.value){
+    if(isValidMoveF(target.value, loc)){
+      markAsCorrect(true, target)
+      //remove wrong-value highlight
+      removeHightlight()
+    }
+    else {
+      markAsCorrect(false, target)
+      //disable solve button
+      document.getElementById('solve-button').disabled = true
+    }
+  }
+  else {
+    target.classList.remove('right-value', 'wrong-value')
+    removeHightlight()
+  }
+  })
 
-var grid2 = [
-  [ 3, 8, 6, 5, 8, 8, 4, 8, 8],
-  [ 5, 2, 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-  ['x', 8, 7, 'x', 'x', 'x', 'x', 3, 1],
-  ['x', 'x', 3, 'x', 1, 'x', 'x', 8, 'x'],
-  [ 9, 'x', 'x', 8, 6, 3, 'x', 'x', 5],
-  ['x', 5, 'x', 'x', 9, 'x', 6, 'x', 'x'],
-  [ 1, 3, 'x', 'x', 'x', 'x', 2, 5, 'x'],
-  ['x', 'x', 'x', 'x', 'x', 'x', 'x', 7, 4],
-  ['x', 'x', 5, 2, 'x', 6, 3, 'x', 'x']
-]
+function removeHightlight () {
+  document.querySelectorAll('.wrong-value').forEach((c) => {
+    c.classList.add('right-value')
+    c.classList.remove('wrong-value')
+  })
+  document.getElementById('solve-button').disabled = false
+}
 
-/*var gridLayout = document.querySelector('#sudoku-grid')
+function markAsCorrect(correct, target){
+  if(correct){
+    target.classList.remove('wrong-value')
+    target.classList.add('right-value')
+  }
+  else {
+    target.classList.remove('right-value')
+    target.classList.add('wrong-value')
+  }
+}
 
-grid.forEach( (arr)=>{gridLayout.innerHTML = gridLayout.innerHTML + '<div>' + arr + '</div>'})
-solve(grid)
-gridLayout.innerHTML = gridLayout.innerHTML + '<h2>Solved Grid:</h2>'
-grid.forEach( (arr)=>{gridLayout.innerHTML = gridLayout.innerHTML + '<div>' + arr + '</div>'})*/
+function isValidMoveF(num, location) {
+  let row = location.row
+  let col = location.col
+
+  return  checkRowF(num, row, col) &&
+          checkColumnF(num, row, col) &&
+          checkBoxF(num, row - (row % 3),  col - (col % 3), row, col)
+}
+
+function checkRowF(num, row, col) {
+  for(let i = 0; i < 9; i++) {
+    if (i == col) continue
+    let cell = document.getElementById('C' + row + i)
+    if(cell.value == num){
+      markAsCorrect(false, cell)
+      return false
+    }
+  }
+  return true
+}
+
+function checkColumnF(num, row, column) {
+  for(let i = 0; i < 9; i++) {
+    if (i == row) continue
+    let cell = document.getElementById('C' + i + column)
+    if( cell.value == num){
+      markAsCorrect(false, cell)
+      return false
+    }
+  }
+  return true
+}
+
+function checkBoxF(num, boxStartRow, boxStartColumn, row, col) {
+  for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+      if (((boxStartRow + i) == row) && ((boxStartColumn + j) == col)) continue
+      let cell = document.getElementById('C' + (boxStartRow + i) + (boxStartColumn + j))
+      if(cell.value == num){
+        markAsCorrect(false, cell)
+        return false
+      }
+    }
+  }
+  return true
+}
 
 function solveSudoku() {
-  var grid= [[],[],[],[],[],[],[],[],[]]
+  let grid= [[],[],[],[],[],[],[],[],[]]
   // get user input
   for(let i = 0; i < 9; i++){
     for(let j = 0; j< 9; j++){
       let cellId = 'C' + i + j
       let cellValue = parseInt(document.getElementById(cellId).value)
       grid[i][j] = isNaN(cellValue) ? 'x' : cellValue
-      //document.getElementById('C' + (i * 9 + j)).value = isNaN(cellValue) ? 'x' : cellValue
     }
   }
   console.log(grid)
   solve(grid)
-  /*let gridLayout = document.querySelector('#solved-grid')
-  gridLayout.innerHTML = gridLayout.innerHTML + '<h2>Solved Grid:</h2>'
-  grid.forEach( (arr)=>{gridLayout.innerHTML = gridLayout.innerHTML + '<div>' + arr + '</div>'})*/
 }
 
 function solve(grid) {
   //solved when no unassigned cell
-  var location = {row: '', col: ''}
+  let location = {row: '', col: ''}
   if(!unassignedCell(grid, location))
     return true
   for(let i = 1; i <= 9 ; i++)
@@ -58,7 +110,6 @@ function solve(grid) {
         //make tentative assignment
         grid[location.row][location.col] = i
         document.getElementById('C' + location.row + location.col).value = i
-        document.getElementById('C' + location.row + location.col).style = 'color:red'
         // return is solved
         if(solve(grid))
           return true
@@ -116,4 +167,15 @@ function checkBox(num, grid, boxStartRow, boxStartColumn) {
     }
   }
   return true
+}
+
+function resetGrid() {
+  document
+  .querySelectorAll('td input')
+  .forEach(c => {
+    c.classList.remove('right-value')
+    c.classList.remove('wrong-value')
+    c.value = ''
+  })
+  document.getElementById('solve-button').disabled = false
 }
